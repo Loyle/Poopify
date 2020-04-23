71621c7f32b71874d6454a8f65f0275fb9d01c2a
theau <theau.zatti@gmail.com>
2020-04-23 18:14:51 +0200

    searchBar Render

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { SettingsComponent } from './settings/settings.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
 imports: [
@@ -14,6 +15,7 @@ imports: [
     AppComponent,
     SidenavComponent,
     SettingsComponent,
+    SearchBarComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
