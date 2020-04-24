4e7a4528b9ccbf1928a7ae4e92ccc85f745aecda
Loyle <antoine.charmeau@laposte.net>
2020-04-24 15:32:50 +0200

    Fondu + peaufinages des bugs + double lecteur

--- src/app/player-bar/player-bar.component.ts
+++ src/app/player-bar/player-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, HostListener} from '@angular/core';
 
 @Component({
 	selector: 'app-player-bar',
@@ -7,24 +7,36 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 })
 export class PlayerBarComponent implements OnInit {
 
-	@ViewChild('playerStateRange') playerStateRange : ElementRef;
-
 	public YT: any;
-	public videoID: any;
-	public player: any;
-	public isPlaying = false;
+	public videoID = "VPRjCeoBqrI";
 
+	public player: any;
+	public playerPassiv : any;
+	public player1: any;
+	public player2: any;
+	public actualPlayer;
+
+	public isPlaying = false;
+	public isFading = false;
+	public fading : any;
+
+	public fadeOut = 10;
 	public duration = 0;
 	public currentTime = 0;
 
 	public isMusicLoaded = false;
+	public volume = 100;
 
 
 	public musicTitle = "";
 	public thumbnail = "";
 
-	public playlist : string[];
-	public listened : string[];
+	public playlist : string[] = [];
+	public playlistPos = 0;
+
+	public listened : string[] = [];
+
+	public playerReady : boolean = false;
 
 	constructor() { }
 	init() {
@@ -36,25 +48,40 @@ export class PlayerBarComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.init();
+		this.initTestPlaylist();
 
 		window['onYouTubeIframeAPIReady'] = (e) => {
 			this.YT = window['YT'];
-			this.player = new window['YT'].Player('player', {
+			this.player1 = new window['YT'].Player('player1', {
 				height: '0',
 				width: '0',
 				suggestedQuality: "hd720",
 				events: {
 					'onStateChange': this.onPlayerStateChange,
 					'onError': this.onPlayerError,
-					'onReady': this.onPlayerReady(this)
+					'onReady': this.onPlayer1Ready(this)
+				}
+			});
+
+			this.player2 = new window['YT'].Player('player2', {
+				height: '0',
+				width: '0',
+				suggestedQuality: "hd720",
+				events: {
+					'onStateChange': this.onPlayerStateChange,
+					'onError': this.onPlayerError,
+					'onReady': this.onPlayer2Ready(this)
 				}
 			});
 		}
 
+		this.player = this.player1;
+		this.playerPassiv = this.player2;
+		this.actualPlayer = 1;
+
 		setInterval(() => {
 			if(!this.isMusicLoaded) {
 				if(this.player.getDuration() > 0) {
-					this.isMusicLoaded = true;
 					this.loadMusicData();
 				}
 			}
@@ -65,46 +92,152 @@ export class PlayerBarComponent implements OnInit {
 	}
 	
 	previousMusic() : void {
-		if(this.currentTime > 2 && this.isPlaying) {
+		if(this.currentTime > 2) {
 			// On remet au debut
 			this.player.seekTo(0);
 		}
 		else {
 			// On charge la musique precedente
+			if(this.playlistPos > 0) {
+				this.playlistPos--;
+				this.videoID = this.playlist[this.playlistPos];
+				this.loadMusic();
+				this.preloadNext();
+				this.isMusicLoaded = false;
+				this.isPlaying = true;
+			}
+			else {
+				console.log("[PLAYLIST] Playlist is already at the beginning");
+				this.player.seekTo(0);
+			}
+
 		}
 	}
-	playPauseMusic(videoID) : void {
+	playPauseMusic(videoID, autoplay = true) : void {
 		if(this.isPlaying) {
-			this.player.pauseVideo(); 
+			this.pauseAllVideo();
 		}
 		else {
-			if(videoID != this.videoID) {
+			if(videoID != this.videoID || this.duration == 0) { // Si la musique est pas load (duration == 0) on la load
 				this.videoID = videoID;
-				this.loadPlayer();
+				this.loadMusic(autoplay);
+				this.preloadNext();
+				this.isMusicLoaded = false;
 			}
-			this.player.playVideo(); 
+
+			if(this.isFading)
+				this.playAllVideo();
+			else
+				this.player.playVideo(); 
 		}
 		this.isPlaying = !this.isPlaying;
 	}
 	nextMusic() : void {
-		alert("Next");
+		this.listened.push(this.videoID);
+		this.playlistPos++;
+
+		if(this.playlist.length > this.playlistPos) {
+			this.videoID = this.playlist[this.playlistPos];
+			this.pauseAllVideo();
+			this.switchPlayers();
+			this.player.playVideo();
+			this.preloadNext();
+			this.isMusicLoaded = false;
+			this.isPlaying = true;
+		}
+		else
+			console.log("[PLAYLIST] Playlist is ended");
 	}
 	volumeChanged(value) : void {
-		this.player.setVolume(value);
+		this.player1.setVolume(value);
+		this.player2.setVolume(value);
+		this.volume = value;
 	}
 	timeChanged(value) : void {
 		this.player.seekTo(value / 1000,true);
 		this.checkPlayerStatus();
 	}
 
-	loadPlayer() : void {
-		this.player.loadVideoByUrl("http://www.youtube.com/v/" + this.videoID + "?version=3" ,0 ,"hd720");
-		this.isMusicLoaded = false;
+	loadMusic(autoplay = true) : void {
+		this.getActualPlayer();
+		this.player.loadVideoByUrl("http://www.youtube.com/v/" + this.videoID + "?version=3&autoplay="+((this.isPlaying && autoplay) ? "1" :"0") ,0 ,"hd720");
 	}
+
+	preloadNext() : void {
+		if(this.playlist.length > this.playlistPos + 1) {
+			var url : string = "http://www.youtube.com/v/" + this.playlist[this.playlistPos + 1] + "?version=3&autoplay=0";
+			this.playerPassiv.stopVideo();
+			this.playerPassiv.loadVideoByUrl(url,0,"hd720");
+			this.playerPassiv.pauseVideo();
+		}
+	}
+
+	getActualPlayer() : void {
+		if(this.actualPlayer == 1) {
+			this.player = this.player1;
+			this.playerPassiv = this.player2;
+		}
+		else {
+			this.player = this.player2;
+			this.playerPassiv = this.player1;
+		}
+	}
+
+	switchPlayers() : void {
+		if(this.actualPlayer == 1)
+			this.actualPlayer = 2;
+		else
+			this.actualPlayer = 1;
+
+		this.getActualPlayer();
+	}
+
+	doFadeOut() : void {
+		this.switchPlayers();
+		this.player.playVideo();
+		this.isMusicLoaded = false;
+		if(this.fadeOut > 0) {	
+			this.isFading = true;
+
+			var upVolume = 0;
+			var downVolume = this.volume;
+			this.fading = setInterval(() => {
+				if(this.isPlaying) {
+					this.playerPassiv.setVolume(downVolume);
+					this.player.setVolume(upVolume);
+
+					upVolume++;
+					downVolume--;
+
+					if(upVolume - 1 > this.volume) {
+						this.isFading = false;
+						clearInterval(this.fading);
+						this.preloadNext();
+					}
+				}
+			}, (1000 * this.fadeOut) / this.volume );
+		}
+		else {
+			this.preloadNext();
+		}
+	}
+
+
 
 	checkPlayerStatus() : void {
 		this.currentTime = this.player.getCurrentTime();
 		this.duration = this.player.getDuration();
+		if(this.duration - this.currentTime <= this.fadeOut) {
+			if(this.isFading == true) {
+				// Il y a deja un fadout en cours !!
+				this.isFading = false;
+				clearInterval(this.fading);
+				this.preloadNext();
+			}
+			this.playlistPos++;
+			this.videoID = this.playlist[this.playlistPos];
+			this.doFadeOut();
+		}
 	}
 
 	loadMusicData() : void {
@@ -113,6 +246,14 @@ export class PlayerBarComponent implements OnInit {
 		this.isMusicLoaded = true;
 	}
 
+	pauseAllVideo() {
+		this.player1.pauseVideo();
+		this.player2.pauseVideo();
+	}
+	playAllVideo() {
+		this.player1.playVideo();
+		this.player2.playVideo();
+	}
 
 	onPlayerStateChange(event) {
 		console.log("Player status changed");
@@ -122,7 +263,35 @@ export class PlayerBarComponent implements OnInit {
 	onPlayerError(event) {
 		alert("Error lecture");
 	}
-	onPlayerReady(t) {
-		console.log("Player is ready");
+	onPlayer1Ready(t) {
+		/*console.log("Player1 is ready");
+		t.player = t.player1;
+		t.actualPlayer = 1;*/
+		
+	}
+	onPlayer2Ready(t) {
+		/*console.log("Player2 is ready");
+		t.playerPassiv = t.player2;
+		t.playPauseMusic(this.playlist[0],false);*/
+	}
+
+	@HostListener('document:keypress', ['$event'])
+	handleKeyboardEvent(event: KeyboardEvent) { 
+		if(event.key == " ") {
+			this.playPauseMusic(this.videoID, true);
+		}
+	}
+
+
+	initTestPlaylist() {
+		this.playlist.push("VPRjCeoBqrI");
+		this.playlist.push("du_urI33fuE");
+		this.playlist.push("Vujit_MkMt8");
+		this.playlist.push("K-865CiZKKE");
+		this.playlist.push("kiEJvdiA4yc");
+		this.playlist.push("CXBdq6YOv_c");
+		this.playlist.push("PGE7RG7wwTc");
+		this.playlist.push("p3l7fgvrEKM");
+		this.playlist.push("iRA82xLsb_w");
 	}
 }
