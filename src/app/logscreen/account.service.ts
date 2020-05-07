203860df945c8e328e25aa543df2a027fab1e350
TheauZatti <50621116+TheauZatti@users.noreply.github.com>
2020-05-07 15:38:56 +0200

    update services

--- src/app/logscreen/account.service.ts
+++ src/app/logscreen/account.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Account } from './account';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AccountService {
+baseUrl = 'http://localhost/api';
+accounts: Account[];
 
-  constructor() { }
+constructor(private http: HttpClient) { }
+
+getAll(): Observable<Account[]> {
+  return this.http.get(`${this.baseUrl}/test`).pipe(
+    map((res) => {
+      this.accounts = res['data'];
+      return this.accounts;
+  }),
+  catchError(this.handleError));
+}
+
+private handleError(error: HttpErrorResponse) {
+  console.log(error);
+  // return an observable with a user friendly message
+  return throwError('Error! something went wrong.');
 }
